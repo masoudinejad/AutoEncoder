a221c324a417769b9402d273b46f401bba92da6e
Mojtaba Masoudinejad <masoudinejad@gmail.com>
2024-04-01 19:30:02 +0200

    use dataloaders

--- train_nuclei.py
+++ train_nuclei.py
@@ -1,31 +1,13 @@
 # %%
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
 import autoencoder.contractive as cae
+from dataloaders import get_loaders
 
 # %%
-# Define a transform to normalize the data
-transform = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        # transforms.Lambda(lambda x: x.view(1, -1)),
-    ]
-)
-train_data_folder = "./data/cell/patch_new/patch_train_no_bg/nuclei_patches/reorganized"
-valid_data_folder = "./data/cell/patch_new/patch_valid_no_bg/nuclei_patches/reorganized"
-test_data_folder = "./data/cell/patch_new/test_no_bg"
-train_dataset = datasets.ImageFolder(root=train_data_folder, transform=transform)
-valid_dataset = datasets.ImageFolder(root=valid_data_folder, transform=transform)
-test_dataset = datasets.ImageFolder(root=test_data_folder, transform=transform)
-
-# Create DataLoader instances for train and validation sets
 batch_size = 64
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=batch_size)
-test_loader = DataLoader(test_dataset, batch_size=batch_size)
+train_loader, valid_loader, test_loader = get_loaders(batch_size=batch_size)
 
 
 # %%
@@ -76,7 +58,7 @@ model = cae.CAE(
 )
 # %%
 epochs = 1
-# model.summary()
+model.summary()
 # model.store_summary()
 model_history = model.fit(
     train_loader, val_loader=valid_loader, epochs=epochs, lr=0.002
@@ -87,5 +69,4 @@ model_history = model.fit(
 
 # torch.save(model, "entire_model.pth")
 
-# %%
-model.visualize_reconstructions(test_loader)
+# model.visualize_reconstructions(test_loader)
